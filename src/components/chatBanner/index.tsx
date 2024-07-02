fa235d9fa48b0603b36e0e69a20d311eb0926ca6
bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2024-07-02 16:29:12 +0530

    cheat meassage design set
    cheat meassage design set

--- src/components/chatBanner/index.tsx
+++ src/components/chatBanner/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DoubelTreuIcon from '../commons/icons/doubelTreuIcon'
+import Image from 'next/image'
 
 const CheatBanner = () => {
     return (
@@ -7,8 +8,9 @@ const CheatBanner = () => {
             <div className="w-full mt-[17px]">
                 <span className='text-sm text-brand-darkGray font-normal text-center w-full flex items-start justify-center  '>8/20/2020</span>
 
-           <div className='flex flex-col items-center'>
-           <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  '>
+           <div className='flex flex-col items-end pr-4'>
+           <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  relative'>
+            <Image src={"/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]'/>
                  
                         <span className='text-sm text-white '>Hi team 👋</span>
                         <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
