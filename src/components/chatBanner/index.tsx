a42680735279aada53d7cbb47a0ed9cf75edde07
bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2024-07-02 17:14:59 +0530

    banner design
    banner design

--- src/components/chatBanner/index.tsx
+++ src/components/chatBanner/index.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import DoubelTreuIcon from "../commons/icons/doubelTreuIcon";
-import Image from "next/image";
+import React from 'react'
+import DoubelTreuIcon from '../commons/icons/doubelTreuIcon'
+import Image from 'next/image'
 
 const CheatBanner = () => {
-  return (
-    <div>
-      <div className="w-full mt-4">
-        <span className="text-sm text-brand-darkGray font-normal text-center w-full flex items-start justify-center  ">
-          8/20/2020
-        </span>
+    return (
+        <div>
+            <div className="w-full mt-[17px]">
+                <span className='text-sm text-brand-darkGray font-normal text-center w-full flex items-start justify-center  '>8/20/2020</span>
 
-              <div className='mb-4'>
+              <div className='mb-12'>
               <div className='flex flex-col items-end pr-4'>
                     <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  relative'>
-                        <Image src={"/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
+                        <Image src={"/assets/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
 
                         <span className='text-sm text-white '>Hi team 👋</span>
                         <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
@@ -22,7 +20,7 @@ const CheatBanner = () => {
 
                     </div>
                     <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  relative mt-[6px]'>
-                        <Image src={"/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px] hidden' />
+                        <Image src={"/assets/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px] hidden' />
 
                         <span className='text-sm text-white '>Anyone on for lunch today</span>
                         <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
@@ -33,9 +31,9 @@ const CheatBanner = () => {
                 </div>
                 <div className='flex items-start gap-4 pl-4 my-4'>
 
-                    <Image src={"/images/cheatavatar1.svg"} alt='img' width={32} height={32} className='' />
+                    <Image src={"/assets/images/cheatavatar1.svg"} alt='img' width={32} height={32} className='' />
                     < div className='bg-[#F2F2F7] rounded-[6px] px-[8px] py-[4px] flex flex-col  relative'>
-                        <Image src={"/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
+                        <Image src={"/assets/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
                         <div className='flex items-center gap-[10px]'>
                             <span className='text-[#2C2C2E] font-semibold text-sm'>Jav</span>
                             <span className='text-xs text-brand-darkGray'>Engineering</span>
@@ -51,7 +49,7 @@ const CheatBanner = () => {
                 </div>
                 <div className='flex flex-col items-end pr-4'>
                     <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  relative'>
-                        <Image src={"/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
+                        <Image src={"/assets/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
 
                         <span className='text-sm text-white '>I am down for whatever!</span>
                         <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
@@ -64,9 +62,9 @@ const CheatBanner = () => {
                 <div className='flex items-start  pl-4 mt-4'>
                     <div className='flex flex-col gap-3'>
                         <div className='flex items-start gap-4'>
-                            <Image src={"/images/cheatavatar2.svg"} alt='img' width={32} height={32} className='' />
+                            <Image src={"/assets/images/cheatavatar2.svg"} alt='img' width={32} height={32} className='' />
                             < div className='bg-[#F2F2F7] rounded-[6px] px-[8px] py-[4px] flex flex-col  relative'>
-                                <Image src={"/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
+                                <Image src={"/assets/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
                                 <div className='flex items-center gap-[10px]'>
                                     <span className='text-[#2C2C2E] font-semibold text-sm'>Aubrey</span>
                                     <span className='text-xs text-brand-darkGray'>Product</span>
@@ -81,15 +79,15 @@ const CheatBanner = () => {
                             </div>
                         </div>
                         <div className='flex items-start gap-4'>
-                            <Image src={"/images/cheatavatar2.svg"} alt='img' width={32} height={32} className='' />
+                            <Image src={"/assets/images/cheatavatar2.svg"} alt='img' width={32} height={32} className='' />
                             < div className='bg-[#F2F2F7] rounded-[6px] px-[8px] py-[4px] flex flex-col  relative'>
-                                <Image src={"/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
+                                <Image src={"/assets/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
                                 <div className='flex items-center gap-[10px]'>
                                     <span className='text-[#2C2C2E] font-semibold text-sm'>Aubrey</span>
                                     <span className='text-xs text-brand-darkGray'>Product</span>
                                 </div>
                                 <div className='flex items-center gap-[4px]'>
-                                    <span className='text-sm text-[#2C2C2E] '>But limited vegan options @Janet!</span>
+                                    <span className='text-sm text-[#2C2C2E] '>But limited vegan options <a href="/" className='text-[#007AFF]'> @Janet</a>!</span>
                                     <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
                                         <span className='text-xs text-brand-darkGray'>11:46 AM</span>
                                     </div>
@@ -102,7 +100,7 @@ const CheatBanner = () => {
                 </div>
                 <div className='flex flex-col items-end pr-4'>
                     <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  relative'>
-                        <Image src={"/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
+                        <Image src={"/assets/images/chateIcon.svg"} alt='img' width={15} height={39} className='absolute top-0 right-[-9px]' />
 
                         <span className='text-sm text-white '>Agreed</span>
                         <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
@@ -115,9 +113,9 @@ const CheatBanner = () => {
                 <div className='flex items-start  pl-4 mt-4'>
                     <div className='flex flex-col gap-3'>
                         <div className='flex items-start gap-4'>
-                            <Image src={"/images/cheatavatar3.svg"} alt='img' width={32} height={32} className='' />
+                            <Image src={"/assets/images/cheatavatar3.svg"} alt='img' width={32} height={32} className='' />
                             < div className='bg-[#F2F2F7] rounded-[6px] px-[8px] py-[4px] flex flex-col  relative'>
-                                <Image src={"/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
+                                <Image src={"/assets/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
                                 <div className='flex items-center gap-[10px]'>
                                     <span className='text-[#2C2C2E] font-semibold text-sm'>Janet</span>
                                     <span className='text-xs text-brand-darkGray'>Engineering</span>
@@ -133,9 +131,9 @@ const CheatBanner = () => {
                             </div>
                         </div>
                         <div className='flex items-start gap-4'>
-                            <Image src={"/images/cheatavatar4.svg"} alt='img' width={32} height={32} className='' />
+                            <Image src={"/assets/images/cheatavatar4.svg"} alt='img' width={32} height={32} className='' />
                             < div className='bg-[#F2F2F7] rounded-[6px] px-[8px] py-[4px] flex flex-col  relative'>
-                                <Image src={"/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
+                                <Image src={"/assets/images/chateIconeGray.svg"} alt='img' width={15} height={39} className='absolute top-0 left-[-9px]' />
                                 <div className='flex items-center gap-[10px]'>
                                     <span className='text-[#2C2C2E] font-semibold text-sm'>Janet</span>
                                     <span className='text-xs text-brand-darkGray'>Product</span>
@@ -154,28 +152,8 @@ const CheatBanner = () => {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bg-[#007AFF] rounded-md px-2 py-1 flex  relative mt-2">
-            <Image
-              src={"/assets/images/chateIcon.svg"}
-              alt="img"
-              width={15}
-              height={39}
-              className="absolute top-0 right-[-9px] hidden"
-            />
-
-            <span className="text-sm text-white ">
-              Anyone on for lunch today
-            </span>
-            <div className="flex items-center justify-end gap-1 mt-5">
-              <span className="text-xs text-white">11:31 AM</span>
-              <DoubelTreuIcon />
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default CheatBanner;
+export default CheatBanner
