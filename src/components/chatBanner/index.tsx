e31d5281fdce28f6aa3eddd5166eaa057750405d
bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2024-07-02 16:21:20 +0530

    cheat meassage set
    cheat meassage set

--- src/components/chatBanner/index.tsx
+++ src/components/chatBanner/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import DoubelTreuIcon from '../commons/icons/doubelTreuIcon'
 
 const CheatBanner = () => {
-  return (
-    <div>
-        <div className="w-full mt-[17px]">
-            <span className='text-sm text-brand-darkGray font-normal text-center w-full '>8/20/2020</span>
+    return (
+        <div>
+            <div className="w-full mt-[17px]">
+                <span className='text-sm text-brand-darkGray font-normal text-center w-full flex items-start justify-center  '>8/20/2020</span>
+
+           <div className='flex flex-col items-center'>
+           <div className='bg-[#007AFF] rounded-[6px] px-[8px] py-[4px] flex  '>
+                 
+                        <span className='text-sm text-white '>Hi team 👋</span>
+                        <div className='flex items-center justify-end gap-[4px] mt-[21px]'>
+                            <span className='text-xs text-white'>11:31 AM</span> <DoubelTreuIcon />
+                            </div>
+                    
+                </div>
+           </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CheatBanner
