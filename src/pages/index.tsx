c76cfc0c76be859b0be7c9ab33f5cd1a44d99477
bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2024-07-02 17:29:36 +0530

    final design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <div className="max-w-[656px] w-full m-auto max-h-[960px] h-full overflow-auto my-28">
+      <div className="max-w-[656px] w-full m-auto max-h-[960px] h-full overflow-auto my-5">
         <Header />
         <CheatBanner />
         <Footer />
