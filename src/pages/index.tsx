753c2833924cdda7f8b5b4ab33523eb3cef06134
bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2024-07-02 16:01:30 +0530

    banner fille add and cheta icon add
    banner fille add and cheta icon add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
+import CheatBanner from "@/components/chatBanner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,7 +9,8 @@ export default function Home() {
   return (
     <main>
       <div className="max-w-[656px] w-full m-auto max-h-[960px] h-full overflow-auto">
-        <Header />
+    <Header/>
+    <CheatBanner />
       </div>
     </main>
   );
